652c965ce2d3a46ec416ae89e385c0f34dce9e7a
tgourdel <github@t.gourdel.fr>
2020-04-06 13:46:17 +0200

    Add datagenerator code

---
+++ datagenerator/src/main/java/com/datagenerator/talend/components/dataset/DataGeneratorDataset.java
@@ -0,0 +1,80 @@
+package com.datagenerator.talend.components.dataset;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.Data;
+import lombok.ToString;
+
+import com.datagenerator.talend.components.datastore.DataGeneratorDatastore;
+
+import org.talend.sdk.component.api.configuration.Option;
+import org.talend.sdk.component.api.configuration.action.Suggestable;
+import org.talend.sdk.component.api.configuration.condition.ActiveIf;
+import org.talend.sdk.component.api.configuration.constraint.Max;
+import org.talend.sdk.component.api.configuration.constraint.Min;
+import org.talend.sdk.component.api.configuration.constraint.Required;
+import org.talend.sdk.component.api.configuration.type.DataSet;
+import org.talend.sdk.component.api.configuration.ui.DefaultValue;
+import org.talend.sdk.component.api.configuration.ui.widget.TextArea;
+import org.talend.sdk.component.api.meta.Documentation;
+import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
+
+import static com.datagenerator.talend.components.service.UIActionService.LOCALE_CHOICES;
+import static org.talend.sdk.component.api.configuration.ui.layout.GridLayout.FormType.ADVANCED;
+
+@Data
+@DataSet("DataGeneratorDataset")
+@Documentation("DataGenerator Dataset")
+@ToString
+@GridLayout({
+        @GridLayout.Row({ "datastore" }),
+        @GridLayout.Row({ "rows" }),
+        @GridLayout.Row({ "fields" })
+})
+@GridLayout(names = ADVANCED, value = { @GridLayout.Row({"customLocale"}),  @GridLayout.Row({"locales"}), @GridLayout.Row({"customSeed"}),  @GridLayout.Row({"seed"}) })
+
+public class DataGeneratorDataset implements Serializable {
+
+    @Option
+    @Documentation("Connection")
+    private DataGeneratorDatastore datastore;
+
+    @Option
+    @Required
+    @DefaultValue("10000")
+    @Min(1)
+    @Max(1000000000)
+    @Documentation("Rows")
+    private Long rows;
+
+    @Option
+    @Required
+    @Documentation("Fields")
+    private List<FieldConfiguration> fields;
+
+    @Option
+    @Required
+    @DefaultValue("false")
+    @Documentation("Custom Locale")
+    private Boolean customLocale;
+
+    @Option
+    @Documentation("Locales")
+    @ActiveIf(target = "customLocale", value = { "true" })
+    @Suggestable(value = LOCALE_CHOICES, parameters = { "../datastore" })
+    private List<String> locales;
+
+    @Option
+    @Required
+    @DefaultValue("false")
+    @Documentation("Custom Seed")
+    private Boolean customSeed;
+
+    @Option
+    @DefaultValue("123456")
+    @Documentation("Seed")
+    @ActiveIf(target = "customSeed", value = { "true" })
+    private Long seed;
+
+}
---
+++ datagenerator/src/main/java/com/datagenerator/talend/components/service/DataGeneratorComponentService.java
@@ -0,0 +1,217 @@
+package com.datagenerator.talend.components.service;
+
+import com.datagenerator.talend.components.dataset.FieldConfiguration;
+import com.github.javafaker.Faker;
+import org.talend.sdk.component.api.record.Record;
+import org.talend.sdk.component.api.service.Service;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class DataGeneratorComponentService {
+
+    public Record.Builder addFieldsToRecord(Integer iterator,
+                                            Faker fake, List<FieldConfiguration> fields, Record.Builder b) {
+
+        Date date = new Date();
+
+        // For each field select we add a value to the record
+        for (FieldConfiguration field : fields) {
+                switch (field.getType()) {
+                    // Personal
+                    case FULLNAME:
+                        b.withString(field.getName(), fake.name().fullName());
+                        break;
+                    case FIRSTNAME:
+                        b.withString(field.getName(), fake.name().firstName());
+                        break;
+                    case LASTNAME:
+                        b.withString(field.getName(), fake.name().lastName());
+                        break;
+                    case MIDDLENAME:
+                        b.withString(field.getName(), fake.name().nameWithMiddle());
+                        break;
+                    case AGE:
+                        b.withInt(field.getName(), fake.number().numberBetween(field.getMin(), field.getMax()));
+                        break;
+                    case SEX:
+                        b.withString(field.getName(), fake.demographic().sex());
+                        break;
+                    case MARITALSTATUS:
+                        b.withString(field.getName(), fake.demographic().maritalStatus());
+                        break;
+                    case EMAIL:
+                        b.withString(field.getName(), fake.internet().emailAddress());
+                        break;
+                    case USERNAME:
+                        b.withString(field.getName(), fake.name().username());
+                        break;
+                    case PASSWORD:
+                        b.withString(field.getName(), fake.internet().password(field.getMin(), field.getMax()));
+                        break;
+                    case DATEOFBIRTH:
+                        b.withDateTime(field.getName(), fake.date().birthday());
+                        break;
+                    case TELEPHONENUMBER:
+                        b.withString(field.getName(), fake.phoneNumber().phoneNumber());
+                        break;
+                    case NATIONALITY:
+                        b.withString(field.getName(), fake.country().name());
+                        break;
+                    case NATIONALITYCODE:
+                        b.withString(field.getName(), fake.country().countryCode2());
+                        break;
+                    case CITY:
+                        b.withString(field.getName(), fake.address().city());
+                        break;
+                    case POSTALCODE:
+                        b.withString(field.getName(), fake.address().zipCode());
+                        break;
+                    case STREETADDRESS:
+                        b.withString(field.getName(), fake.address().streetAddress());
+                        break;
+                    case STREETNUMBER:
+                        b.withString(field.getName(), fake.address().streetAddressNumber());
+                        break;
+                    case APPARTNUMBER:
+                        b.withString(field.getName(), fake.address().buildingNumber());
+                        break;
+                    case CARDNUMBER:
+                        b.withString(field.getName(), fake.business().creditCardNumber());
+                        break;
+                    case EXPIRYDATE:
+                        b.withString(field.getName(), fake.business().creditCardExpiry());
+                        break;
+                    case VENDOR:
+                        b.withString(field.getName(), fake.business().creditCardType());
+                        break;
+                    case PASSPORTNUMBER:
+                        b.withString(field.getName(), fake.regexify("[A-Z]{1}[0-9]{7}"));
+                        break;
+                    case COMPANYDOMAIN:
+                        b.withString(field.getName(), fake.company().industry());
+                        break;
+                    case COMPANYNAME:
+                        b.withString(field.getName(), fake.company().name());
+                        break;
+                    case COMPANYURL:
+                        b.withString(field.getName(), fake.company().url());
+                        break;
+                    case ISBN10:
+                        b.withString(field.getName(), fake.code().isbn10());
+                        break;
+                    case ISBN13:
+                        b.withString(field.getName(), fake.code().isbn13());
+                        break;
+                    case IMEI:
+                        b.withString(field.getName(), fake.code().imei());
+                        break;
+                    case ASIN:
+                        b.withString(field.getName(), fake.code().asin());
+                        break;
+                    case EAN8:
+                        b.withString(field.getName(), fake.code().ean8());
+                        break;
+                    case EAN13:
+                        b.withString(field.getName(), fake.code().ean13());
+                        break;
+                    case GTIN8:
+                        b.withString(field.getName(), fake.code().gtin8());
+                        break;
+                    case GTIN13:
+                        b.withString(field.getName(), fake.code().gtin13());
+                        break;
+                    case FREETEXT:
+                        b.withString(field.getName(), field.getFreetext());
+                        break;
+                    // Random
+                    case RANDOMINTBETWEEN:
+                        b.withInt(field.getName(), fake.number().numberBetween(field.getMin(), field.getMax()));
+                        break;
+                    case RANDOMSTRING:
+                        b.withString(field.getName(), fake.regexify("(\\w){" + field.getMin().toString() + "," + field.getMax().toString() + "}"));
+                        break;
+                    case RANDOMBOOLEAN:
+                        b.withBoolean(field.getName(), fake.bool().bool());
+                        break;
+                    case RANDOMINT:
+                        b.withInt(field.getName(), (int) fake.number().randomNumber(field.getLength(), true));
+                        break;
+                    case INCREMENTALINT:
+                        b.withInt(field.getName(), (iterator * field.getIncrement()) + field.getMin());
+                        break;
+                    case CUSTOM:
+                        b.withString(field.getName(), fake.regexify(field.getRegex()));
+                        break;
+                    // Dates
+                    case CURRENTDATETIME:
+                        b.withDateTime(field.getName(), date);
+                        break;
+                    case CURRENTTIMESTAMP:
+                        b.withLong(field.getName(), date.getTime());
+                        break;
+                    case RANDOMDATEBETWEEN:
+                        b.withDateTime(field.getName(), fake.date().between(java.sql.Date.valueOf(field.getStartTime()), java.sql.Date.valueOf(field.getEndTime())));
+                        break;
+                    // Funny
+                    case BEER:
+                        b.withString(field.getName(), fake.beer().name());
+                        break;
+                    case BEERSTYLE:
+                        b.withString(field.getName(), fake.beer().style());
+                        break;
+                    case BOOKGENRE:
+                        b.withString(field.getName(), fake.book().genre());
+                        break;
+                    case BOOKTITLE:
+                        b.withString(field.getName(), fake.book().title());
+                        break;
+                    case BOOKAUTHOR:
+                        b.withString(field.getName(), fake.book().author());
+                        break;
+                    case BOOKPUBLISHER:
+                        b.withString(field.getName(), fake.book().publisher());
+                        break;
+                    case APPNAME:
+                        b.withString(field.getName(), fake.app().name());
+                        break;
+                    case APPVERSION:
+                        b.withString(field.getName(), fake.app().version());
+                        break;
+                    case TEMPERATURECELSIUS:
+                        b.withString(field.getName(), fake.weather().temperatureCelsius(field.getMin(), field.getMax()));
+                        break;
+                    case TEMPERATUREFAHRENHEIT:
+                        b.withString(field.getName(), fake.weather().temperatureFahrenheit(field.getMin(), field.getMax()));
+                        break;
+                    case WEATHER:
+                        b.withString(field.getName(), fake.weather().description());
+                        break;
+                    case ANIMAL:
+                        b.withString(field.getName(), fake.animal().name());
+                        break;
+                    case AIRCRAFT:
+                        b.withString(field.getName(), fake.aviation().aircraft());
+                        break;
+                    case AIRPORT:
+                        b.withString(field.getName(), fake.aviation().airport());
+                        break;
+                    case FILENAME:
+                        b.withString(field.getName(), fake.file().fileName());
+                        break;
+                    case FILEEXTENSION:
+                        b.withString(field.getName(), fake.file().extension());
+                        break;
+                    case COLORNAME:
+                        b.withString(field.getName(), fake.color().name());
+                        break;
+                    case COLORHEX:
+                        b.withString(field.getName(), fake.color().hex());
+                        break;
+                    default:  b.withString(field.getName(),"error: field type not found");
+                }
+        }
+        return b;
+    }
+}
